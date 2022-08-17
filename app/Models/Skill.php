3250aea8c65f2702a149e51ee0714b3fbd73bbc4
Akhtar Munir <akhtarmunir.cs@gmail.com>
2022-08-17 22:12:45 +0500

    models updated

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Skill extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function attachUsers($users)
+    {
+        return $this->users()->attach($users);
+    }
+
+    public function detachUsers($users)
+    {
+        return $this->users()->detach($users);
+    }
+
+    public function syncUsers($users)
+    {
+        return $this->users()->sync($users);
+    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->boolean('admin')->default(0);
             $table->string('code');
             $table->string('name');
             $table->string('email')->unique();
