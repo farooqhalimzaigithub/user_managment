bc77bc9e62bb66727dc0988db9f41615b0f4ff02
Akhtar Munir <akhtarmunir.cs@gmail.com>
2022-08-17 21:16:22 +0500

    migration updated

--- app/Models/SkillUser.php
+++ app/Models/SkillUser.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UserSkill extends Model
+class SkillUser extends Model
 {
     use HasFactory;
 }
--- database/migrations/2022_08_17_161401_create_skill_user_table.php
+++ database/migrations/2022_08_17_161401_create_skill_user_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('user_skills', function (Blueprint $table) {
-            $table->id();
+        Schema::create('skill_user', function (Blueprint $table) {
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('skill_id');
             $table->timestamps();
 
+            $table->primary(['user_id','skill_id']);
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('skill_id')->references('id')->on('skills')->onDelete('cascade');
         });
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_skills');
+        Schema::dropIfExists('skill_user');
     }
 };
